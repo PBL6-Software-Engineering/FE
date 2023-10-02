3d0ec07c06bd5e64ab53ea1f5c70310b2415aba4
Tran Thanh Nguyen <trannguyen.06042002@gmail.comn>
2023-10-02 16:23:01 +0700

    issue #6 #7 v2

--- health-care-web/src/app/base/auth/sign-in/sign-in-doctor/sign-in-doctor.component.ts
+++ health-care-web/src/app/base/auth/sign-in/sign-in-doctor/sign-in-doctor.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in-doctor.component.scss']
 })
 export class SignInDoctorComponent {
-
+  hideShowPass(){
+    let passField = document.getElementById("password");
+    let toggleButton = document.getElementById("toggleButton");
+    if(toggleButton?.getAttribute("src")=="assets/media/icon/eye-off.svg")
+      toggleButton?.setAttribute("src","assets/media/icon/eye-solid.svg");
+    else {
+      toggleButton?.setAttribute("src","assets/media/icon/eye-off.svg");
+    }
+    if (passField != null){
+      if(passField.getAttribute("type") =="text"){
+        passField.setAttribute("type","password");
+      }
+      else {
+        passField.setAttribute("type","text");
+      }
+    }
+  }
 }
--- health-care-web/src/app/base/auth/sign-up/sign-up-hospital/sign-up-hospital.component.ts
+++ health-care-web/src/app/base/auth/sign-up/sign-up-hospital/sign-up-hospital.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up-hospital.component.scss']
 })
 export class SignUpHospitalComponent {
-
+  hideShowPass(){
+    let passFields = document.getElementsByClassName("passwordInp");
+    if (passFields!= null){
+      for(let i = 0 ; i < passFields.length ; i++){
+        if(passFields[i].getAttribute("type") =="text"){
+          passFields[i].setAttribute("type","password");
+        }
+        else {
+          passFields[i].setAttribute("type","text");
+        }
+   
+      }
+          }
+  }
 }
