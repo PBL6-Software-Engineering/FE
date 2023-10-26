fd88b66608a11ee43c1bdfc44c4fb4bbd0c89c46
Tran Thanh Nguyen <trannguyen.06042002@gmail.comn>
2023-10-26 22:38:49 +0700

    issue#16+categories

---
+++ health-care-web/src/app/user/features/f4-categories/f4-categories.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { UserService } from '../services/user.service';
+
+@Component({
+  selector: 'app-f4-categories',
+  templateUrl: './f4-categories.component.html',
+  styleUrls: ['./f4-categories.component.scss']
+})
+export class F4CategoriesComponent {
+
+}
