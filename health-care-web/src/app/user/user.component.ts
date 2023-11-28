3fd32d742093abd47625e7f6fc49bf9a26d3f5bc
DAITOAN <daitoan2000@gmail.com>
2023-11-28 12:52:32 +0700

    open comment

--- health-care-web/src/app/user/user.component.ts
+++ health-care-web/src/app/user/user.component.ts
@@ -10,35 +10,35 @@ export class UserComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    // const category = this.route.snapshot.data['category'];
-    // const department = this.route.snapshot.data['department'];
-    // const articleOutstanding = this.route.snapshot.data['articleOutstanding'];
-    // const province = this.route.snapshot.data['province'];
-    // const hospitalOutStanding = this.route.snapshot.data['hospitalOutStanding'];
+    const category = this.route.snapshot.data['category'];
+    const department = this.route.snapshot.data['department'];
+    const articleOutstanding = this.route.snapshot.data['articleOutstanding'];
+    const province = this.route.snapshot.data['province'];
+    const hospitalOutStanding = this.route.snapshot.data['hospitalOutStanding'];
 
-    // localStorage.setItem(
-    //   'categories',
-    //   JSON.stringify(category ? category.data || '[]' : '[]'),
-    // );
-    // localStorage.setItem(
-    //   'departments',
-    //   JSON.stringify(department ? department.data || '[]' : '[]'),
-    // );
-    // localStorage.setItem(
-    //   'articlesOutstanding',
-    //   JSON.stringify(
-    //     articleOutstanding ? articleOutstanding.data.data || '[]' : '[]',
-    //   ),
-    // );
-    // localStorage.setItem(
-    //   'hospitalsOutStanding',
-    //   JSON.stringify(
-    //     hospitalOutStanding ? hospitalOutStanding.data.data || '[]' : '[]',
-    //   ),
-    // );
-    // localStorage.setItem(
-    //   'provinces',
-    //   JSON.stringify(province ? province.provinces || '[]' : '[]'),
-    // );
+    localStorage.setItem(
+      'categories',
+      JSON.stringify(category ? category.data || '[]' : '[]'),
+    );
+    localStorage.setItem(
+      'departments',
+      JSON.stringify(department ? department.data || '[]' : '[]'),
+    );
+    localStorage.setItem(
+      'articlesOutstanding',
+      JSON.stringify(
+        articleOutstanding ? articleOutstanding.data.data || '[]' : '[]',
+      ),
+    );
+    localStorage.setItem(
+      'hospitalsOutStanding',
+      JSON.stringify(
+        hospitalOutStanding ? hospitalOutStanding.data.data || '[]' : '[]',
+      ),
+    );
+    localStorage.setItem(
+      'provinces',
+      JSON.stringify(province ? province.provinces || '[]' : '[]'),
+    );
   }
 }
