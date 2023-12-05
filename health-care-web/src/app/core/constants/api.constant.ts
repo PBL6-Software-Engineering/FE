3eab5e4c1a1942edc3e65098dc7c19011e1b46e6
DAITOAN <daitoan2000@gmail.com>
2023-12-05 19:15:48 +0700

    open comment

--- health-care-web/src/app/core/constants/api.constant.ts
+++ health-care-web/src/app/core/constants/api.constant.ts
@@ -1,14 +1,12 @@
 export const linkApi = 'https://vanmanh.azurewebsites.net/api';
 export const prefixApi = 'https://vanmanh.azurewebsites.net';
 
-// export const linkApiChat =
-//   'https://backend-chat-socket-production.up.railway.app/api/v1';
+export const linkApiChat =
+  'https://backend-chat-socket-production.up.railway.app/api/v1';
+export const linkSocket =
+  'https://backend-chat-socket-production.up.railway.app/';
 
-export const linkApiChat = 'http://localhost:3000/api/v1';
-
-// export const linkSocket =
-//   'https://backend-chat-socket-production.up.railway.app/';
-
-export const linkSocket = 'http://localhost:3000';
+// export const linkApiChat = 'http://localhost:3000/api/v1';
+// export const linkSocket = 'http://localhost:3000';
 // export const linkApi = 'http://192.168.198.250:8000/api';
 // export const prefixApi = 'http://192.168.198.250:8000';
